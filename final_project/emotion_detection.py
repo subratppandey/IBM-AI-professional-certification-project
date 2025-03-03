c3578078b45a6668faded9c2241e75a2256d3a8b
Subrat Pandey <subratpandey974@gmail.com>
2025-03-03 10:50:35 -0600

    retrieved dominant emotion from emotion predictions

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,10 +1,15 @@
 import requests
 
-def emotion_detector(text_to_analyze):
+import json
+
+def emotion_detector(text_to_analyse):
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}    
-    payload = {{"raw_document": {"text": text_to_analyze}}
-    
-    response = requests.post(url, headers=headers, json=payload)
-    
-    return response.text
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = payload, headers = headers)
+    res = json.loads(response.text)
+    formatted_response = res['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+    formatted_response['dominant_emotion'] = dominant_emotion
+    return formatted_response 
